2560d3343fb5ce3cae60efe002a5b8810bf99a53
Paulo Freitas <paulofreitas.dev@gmail.com>
2026-05-20 10:24:26 -0300

    player input system
    O Player Input System já foi implementado e validade, onde o PlayerInputReader agora lê, armazena e disponibiliza o valor confiável do input horizontal do jogador para outros scripts, apenas para leitura e não escrita, criando um módulo central de input, preservando arquitetura limpa e expansível para o sistema.

--- Stormblade_GhostRonin_MVP/Assets/_Project/Scripts/Player/PlayerInputReader.cs
+++ Stormblade_GhostRonin_MVP/Assets/_Project/Scripts/Player/PlayerInputReader.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerInputReader : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Input Actions")]
+    [SerializeField] private InputActionReference moveAction;
+
+    [Header("Debug")]
+    [SerializeField] private float moveInputX;
+
+    public float MoveInputX => moveInputX;
+
+    private void OnEnable()
     {
-        
+        if(moveAction != null)
+        {
+            moveAction.action.Enable();
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        if(moveAction != null)
+        {
+            moveAction.action.Disable();
+        }
     }
+
+    private void Update()
+    {
+        if (moveAction == null)
+        {
+            moveInputX = 0f;
+            return;
+        }
+
+        Vector2 moveValue = moveAction.action.ReadValue<Vector2>();
+
+        moveInputX = moveValue.x;
+    }
+
 }
