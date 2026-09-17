e071eb9e183f3913c725aad6c6b4e774d482cf8a
Paulo Freitas <160852863+paulofreitas-dev@users.noreply.github.com>
2026-09-17 16:02:20 -0300

    Criando a estrutura das cutscenes e transição entre as telas

--- Stormblade_GhostRonin_MVP/Assets/_Project/Scripts/UI/Cutscene/CustceneController.cs
+++ Stormblade_GhostRonin_MVP/Assets/_Project/Scripts/UI/Cutscene/CustceneController.cs
@@ -1,16 +1,64 @@
 using UnityEngine;
+using System;
 
 public class CustceneController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Serializable] 
+    private class CutsceneStep
     {
-        
+        public GameObject screen;
+
+        [TextArea(2, 6)]
+        public string text;
     }
 
-    // Update is called once per frame
-    void Update()
+    [Header("Cutscene")]
+    [SerializeField] private CutsceneStep[] steps;
+
+    [Header("References")]
+    [SerializeField] private TypewriterTextUI typewriter;
+
+    private int currentStepIndex;
+
+    private void Start()
     {
+        ShowStep(0);
+    }
+
+    private void ShowStep(int index)
+    {
+        if(index < 0 || index >= steps.Length)
+            return;
+
+        for(int i = 0; i < steps.Length; i++)
+        {
+            if(steps[i].screen != null)
+                steps[i].screen.SetActive(i == index);
+        }
+
+        currentStepIndex = index;
+
+        if(typewriter != null)
+            typewriter.Play(steps[index].text);
+    }
+
+    public void RequestAdvance()
+    {
+        if(typewriter != null && typewriter.IsRevealing)
+        {
+            typewriter.CompleteImmediately();
+            return;
+        }
+
+        int nextStepIndex = currentStepIndex + 1;
+
+        if (nextStepIndex >= steps.Length)
+        {
+            Debug.Log("CutsceneController: último quadro alcançado.");
+
+            return;
+        }
         
+        ShowStep(nextStepIndex);
     }
 }
